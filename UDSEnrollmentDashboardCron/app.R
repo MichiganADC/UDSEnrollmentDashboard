a969d01bb9a6961d063db2680398181c4298c327
L. D. Nicolas May <LDNicolasMay@gmail.com>
2018-04-17 12:40:01 -0400

    adapt app to use linux cron for faster loading times

---
+++ UDSEnrollmentDashboardCron/app.R
@@ -0,0 +1,435 @@
+# app.R
+
+# # # # # 
+## Load libraries ---- 
+
+package.list <- c("shiny", "shinydashboard", "DT", "ggplot2", "RCurl", 
+                  "jsonlite", "dplyr", "tidyr", "lubridate", "forcats",
+                  "ggmap", "maps", "mapdata", "zipcode")
+new.package.list <- 
+  package.list[!(package.list %in% installed.packages()[, "Package"])]
+if (length(new.package.list)) { 
+  install.packages(new.package.list,
+                   repos = "https://cloud.r-project.org/",
+                   verbose = TRUE) 
+}
+
+library(shiny)
+library(shinydashboard)
+library(DT)
+library(ggplot2)
+library(RCurl)
+library(jsonlite)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(forcats)
+library(ggmap)
+library(maps)
+library(mapdata)
+library(zipcode)
+
+# operational <- TRUE
+deployed <- TRUE
+dt_options <- list(paging = FALSE,
+                   searching = FALSE,
+                   ordering = FALSE,
+                   info = FALSE)
+
+# # # # #
+## Source files ---- 
+
+# if (operational) {
+#   # df_mindset_xfrm <- readRDS("df_mindset_xfrm.Rds")
+#   # source("build_lst_summ_tbls.R", local = TRUE)
+#   # source("build_data_plots.R", local = TRUE)
+#   # source("build_lst_map_dfs.R", local = TRUE)
+#   source("helper_fxns_plots.R", local = TRUE)
+# } else {
+#   # df_mindset_xfrm <- readRDS("./UDSEnrollmentDashboardCron/df_mindset_xfrm.Rds")
+#   # source("./UDSEnrollmentDashboardCron/build_lst_summ_tbls.R", local = TRUE)
+#   # source("./UDSEnrollmentDashboardCron/build_data_plots.R", local = TRUE)
+#   # source("./UDSEnrollmentDashboardCron/build_lst_map_dfs.R", local = TRUE)
+#   source("./UDSEnrollmentDashboardCron/helper_fxns_plots.R", local = TRUE)
+# }
+
+if (deployed) {
+  path_to_app <-
+    "~/ShinyApps/MADCDashboard/" # Michigan Medicine R Shiny server
+} else {
+  path_to_app <-
+    "~/Documents/GitHub/UDSEnrollmentDashboard/UDSEnrollmentDashboardCron/" # local
+}
+
+source(paste0(path_to_app, "helper_fxns_plots.R"), local = TRUE)
+
+# ## List for summary tables
+# lst_summ_tbls <- build_lst_summ_tbls(df_mindset_xfrm)
+# 
+# ## df for plots
+# data_plots <- build_data_plots(df_mindset_xfrm)
+# 
+# ## List for maps
+# lst_map_dfs <- build_lst_map_dfs(df_mindset_xfrm)
+
+## ui ---- 
+ui <- dashboardPage( 
+  
+  ## Dashboard skin color
+  skin = "blue", 
+  
+  ## Header ----
+  dashboardHeader(title = "MADC Dashboard"),
+  
+  ## Sidebar ----
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem(text = "Summary", tabName = "summary", icon = icon("table")),
+      menuItem(text = "Plots", tabName = "plots", icon = icon("signal")),
+      menuItem(text = "Maps", tabName = "maps", icon = icon("map"))
+    ) # end sidebarMenu
+  ), # end dashboardSidebar
+  
+  ## Body ----
+  dashboardBody(
+    ## Set colors of font awesome icons
+    tags$style(".fa-dashboard {color:#064193}"),
+    tags$style(".fa-tint {color:#064193}"),
+    tags$style(".fa-magnet {color:#064193}"),
+    
+    ## Tab container ---
+    tabItems(
+      tabItem( # start tabItem 1 for summary tables
+        tabName = "summary",
+        h2("Summary Tables"),
+        fluidRow( # start fluidRow for valueBoxes
+          # valueBox 1,2,3 here
+        ), # end fluidRow for valueBoxes
+        fluidRow( box(width = 12, h3("UDS Version"), 
+                      DT::dataTableOutput("uds_vers")) ), 
+        fluidRow( box(width = 12, h3("Sex"), 
+                      DT::dataTableOutput("sex")) ), 
+        fluidRow( box(width = 12, h3("Race"),
+                      DT::dataTableOutput("race")) ), 
+        fluidRow( box(width = 12, h3("Research"),
+                      DT::dataTableOutput("rsrch")) ), 
+        fluidRow( box(width = 12, h3("Sex + Race"),
+                      DT::dataTableOutput("sex_race")) ), 
+        fluidRow( box(width = 12, h3("UDS Version + Research"),
+                      DT::dataTableOutput("uds_rsrch")) ), 
+        fluidRow( box(width = 12, h3("Sex + MRI Yes"),
+                      DT::dataTableOutput("sex_mri_yes")) ),
+        fluidRow( box(width = 12, h3("Race + MRI Yes"),
+                      DT::dataTableOutput("race_mri_yes")) )
+      ), # end tabItem 1 for summary tables
+      tabItem( # start tabItem 2 for plots
+        tabName = "plots",
+        h2("Cumulative Enrollments"),
+        fluidRow(
+          tabBox(
+            width = 12,
+            title = "Cumulative Enrollments",
+            id = "tabset1",
+            height = "550px",
+            tabPanel(
+              title = "Total",
+              box(width = 12,
+                  plotOutput(outputId = "plot_cum_total"))
+            ), # end tabPanel 1 -- Total plot
+            tabPanel(
+              title = "Sex",
+              box(width = 12,
+                  plotOutput(outputId = "plot_cum_sex"))
+            ), # end tabPanel 2 -- Sex plot
+            tabPanel(
+              title = "Race",
+              box(width = 12,
+                  plotOutput(outputId = "plot_cum_race"))
+            ) # end tabPanel 3 -- Race plot
+          ) # end tabBox 1 for cumulative plots
+        ),
+        fluidRow(
+          box( # start box 1 for date range input (cumulative)
+            width = 12,
+            dateRangeInput(inputId = "dateRange1",
+                           label = "Date range input: yyyy-mm-dd",
+                           start = as.Date("2017-03-01"), end = Sys.Date())
+          )
+        ),
+        fluidRow(
+          tabBox(
+            width = 12,
+            title = "Targer Enrollment by Diagnosis",
+            id = "tabset2",
+            height = "550px",
+            tabPanel("NL",
+                     box(width = 12,
+                         plotOutput(outputId = "plot_cum_dx_target_nl"))),
+            tabPanel("MCI",
+                     box(width = 12,
+                         plotOutput(outputId = "plot_cum_dx_target_mci"))),
+            tabPanel("AD",
+                     box(width = 12,
+                         plotOutput(outputId = "plot_cum_dx_target_ad"))),
+            tabPanel("LBD",
+                     box(width = 12,
+                         plotOutput(outputId = "plot_cum_dx_target_lbd"))),
+            tabPanel("FTD",
+                     box(width = 12,
+                         plotOutput(outputId = "plot_cum_dx_target_ftd")))
+          ) # end tabBox 2 for target diagnosis plots
+        ), # end fluidRow for plots
+        fluidRow(
+          box( # start box 2 for date range input (diagnosis)
+            width = 12,
+            dateRangeInput(inputId = "dateRange2",
+                           label = "Date range input: yyyy-mm-dd",
+                           start = as.Date("2017-03-01"),
+                           end = as.Date("2022-03-01")
+            )
+          )
+        ) # end fluidRow for interactive dates
+      ), # end tabItem 2 for plots
+      tabItem( # start tabItem 3 for maps
+        tabName = "maps",
+        h2("Maps"),
+        fluidRow(
+          tabBox(
+            width = 12,
+            title = "",
+            id = "map_tabset",
+            height = "700px",
+            side = "left",
+            tabPanel(
+              title = "County",
+              box(
+                width = 12,
+                height = "625px",
+                plotOutput(outputId = "map_partic_by_county")
+              )
+            ), # end tabPanel 1 -- county map
+            tabPanel(
+              title = "ZIP",
+              box(
+                width = 12,
+                height = "625px",
+                plotOutput(outputId = "map_partic_by_zip")
+              )
+            ) # end tabPanel 2 -- ZIP map
+          ) # end tabBox
+        ) # end fluidRow
+      ) # end tabItem 3 for maps
+    ) # end tabItems
+  ) # end dashboardBody
+) # end dashboardPage
+
+server <- function(input, output, session) {
+  
+  invalidation_time <- 1000 * 60 * 60 * 6 # 6-hour refresh
+  #                    ^      ^    ^    ^
+  #                    |      |    |    |> 6 hr
+  #                    |      |    |> 60 min / hr
+  #                    |      |> 60 sec / min
+  #                    |> 1000 ms / sec
+  # invalidation_time <- 1000 * 60 * 5 # 5-minute refresh (debug)
+  
+  # # # # # 
+  ## Get data ----
+  
+  # ## Reactive df for core data
+  # data_mindset_rctv <- reactive({
+  #   invalidateLater(invalidation_time, session)
+  #   get_data_mindset() # source: etl_mindset_uds2.R
+  # })
+  # 
+  # ## List for summary tables
+  # lst_summ_tbls <- reactive({
+  #   invalidateLater(invalidation_time, session)
+  #   build_lst_summ_tbls( data_mindset_rctv() )
+  # })
+  # 
+  # ## df for plots
+  # data_plots <- reactive({
+  #   invalidateLater(invalidation_time, session)
+  #   build_data_plots( data_mindset_rctv() )
+  # })
+  # 
+  # ## List for maps
+  # lst_map_dfs <- reactive({
+  #   invalidateLater(invalidation_time, session)
+  #   build_lst_map_dfs( data_mindset_rctv() )
+  # })
+  
+  ## Read in df for core data
+  # df_mindset_xfrm <- 
+  #   reactiveFileReader(intervalMillis = invalidation_time,
+  #                      filePath = "df_mindset_xfrm.Rds",
+  #                      readFunc = readRDS)
+  
+  ## List for summary tables
+  lst_summ_tbls <-
+    reactiveFileReader(intervalMillis = invalidation_time,
+                       filePath = "./rds/lst_summ_tbls.Rds",
+                       readFunc = readRDS)
+  
+  ## df for plots
+  data_plots <-
+    reactiveFileReader(intervalMillis = invalidation_time,
+                       filePath = "./rds/data_plots.Rds",
+                       readFunc = readRDS)
+  
+  ## List for maps
+  lst_map_dfs <-
+    reactiveFileReader(intervalMillis = invalidation_time,
+                       filePath = "./rds/lst_map_dfs.Rds",
+                       readFunc = readRDS)
+  
+  # ## List for summary tables
+  # lst_summ_tbls <- build_lst_summ_tbls(df_mindset_xfrm)
+  # 
+  # ## df for plots
+  # data_plots <- build_data_plots(df_mindset_xfrm)
+  # 
+  # ## List for maps
+  # lst_map_dfs <- build_lst_map_dfs(df_mindset_xfrm)
+  
+  # # # # # 
+  ## Render tables ----
+  
+  ## Use `observe` + `lapply` to render all the summary tables
+  summ_tbl_names <- c("uds_vers", "sex", "race", "sex_race", 
+                      "rsrch", "uds_rsrch", "sex_mri_yes", "race_mri_yes")
+  observe({
+    lapply(summ_tbl_names, function(tbl_name) {
+      output[[tbl_name]] <- DT::renderDataTable({
+        DT::datatable( lst_summ_tbls()[[paste0(tbl_name, "_tbl")]], 
+                       options = dt_options )
+      })
+    }) # end `lapply`
+  }) # end `observe`
+  
+  ## Example of a single table render
+  # output$data_mindset_tbl <- DT::renderDataTable({ 
+  #   DT::datatable( data_mindset_rctv(), options = dt_options ) 
+  # })
+  
+  # # # # # 
+  ## Render plots ----
+  
+  ## Cumulative enrollment totals plot
+  output$plot_cum_total <- renderPlot({
+    cum_plot(df = data_plots(),
+             x = "exam_date", y = "total_cum_sum",
+             plot_title = "Total Participants Over Time",
+             start_date = as.Date(input$dateRange1[1]),
+             end_date = as.Date(input$dateRange1[2]))
+  })
+  
+  ## Cumulative enrollment by sex plot
+  output$plot_cum_sex <- renderPlot({
+    cum_plot_single_grp(df = data_plots(),
+                        x = "exam_date", y = "sex_cum_sum",
+                        group_var = "sex_value",
+                        plot_title = "Participants Over Time by Sex",
+                        start_date = as.Date(input$dateRange1[1]),
+                        end_date = as.Date(input$dateRange1[2]))
+  })
+  
+  ## Cumulative enrollment by race plot
+  output$plot_cum_race <- renderPlot({
+    cum_plot_single_grp(df = data_plots(),
+                        x = "exam_date", y = "race_cum_sum",
+                        group_var = "race_value",
+                        plot_title = "Participants Over Time by Race",
+                        start_date = as.Date(input$dateRange1[1]),
+                        end_date = as.Date(input$dateRange1[2]))
+  })
+  
+  ## Cumulative enrollment by diagnosis vs. diagnosis targets
+  # Use `observe` + `lapply` to render all the target diagnosis plots
+  diagnosis_abbrevs <- c("AD", "FTD", "LBD", "MCI", "NL")
+  observe({
+    lapply(diagnosis_abbrevs, function(dx_abrv) {
+      output[[paste0("plot_cum_dx_target_", tolower(dx_abrv))]] <-
+        renderPlot({
+          cum_plot_dx_target_dx(df = data_plots(),
+                                x = "exam_date", y = "dx_cum_sum",
+                                group_var = "uds_dx",
+                                dx = dx_abrv, 
+                                dx_target = paste0(dx_abrv, " target"),
+                                plot_title = paste0(dx_abrv, " vs. ", 
+                                                    dx_abrv, " Target"),
+                                start_date = as.Date(input$dateRange2[1]),
+                                end_date = as.Date(input$dateRange2[2]))
+        }) # end renderPlot
+    }) # end lapply
+  }) # end observe
+  
+  # # # # # 
+  ## Render maps ----
+  
+  ## Participation by county map
+  county_max <- reactive({
+    max(lst_map_dfs()$map_df_partic_ct_mi_county$Count, na.rm = TRUE)
+  })
+  output$map_partic_by_county <- renderPlot({
+    lst_map_dfs()$mi_base_map +
+      geom_polygon(data = lst_map_dfs()$map_df_partic_ct_mi_county,
+                   aes(fill = Count),
+                   color = "black", size = 0.1) +
+      geom_polygon(color = "black", fill = NA) +
+      theme_bw() +
+      theme(
+        axis.text = element_blank(),
+        axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        axis.title = element_blank()
+      ) +
+      scale_fill_continuous(low = "#eeeeee", high = "royalblue",
+                            breaks = seq(1, county_max(),
+                                         by = (county_max()-1)/5)) +
+      ggtitle(label = "Participant Counts by County",
+              subtitle = "March 2017 to Present")
+  }, height = 600)
+  
+  # ## Participation by ZIP map
+  zip_max <- reactive({
+    max(lst_map_dfs()$map_df_partic_ct_mi_zip$Count, na.rm = TRUE)
+  })
+  output$map_partic_by_zip <- renderPlot({
+    lst_map_dfs()$mi_base_map +
+      geom_point(data = lst_map_dfs()$map_df_partic_ct_mi_zip,
+                 aes(x = longitude, y = latitude, group = zip,
+                     size = Count,
+                     fill = Count),
+                 color = "black", pch = 21) +
+      geom_polygon(color = "black", fill = NA) +
+      theme_bw() +
+      theme(
+        axis.text = element_blank(),
+        axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        axis.title = element_blank()
+      ) +
+      scale_fill_continuous(low = "#eeeeee", high = "royalblue",
+                            breaks = seq(1, zip_max(), 
+                                         by = round((zip_max()-1)/5))) +
+      guides(fill = guide_legend(), size = guide_legend()) +
+      scale_size_continuous(limits = c(1, zip_max()),
+                            breaks = seq(1, zip_max(),
+                                         by = round((zip_max()-1)/5))) +
+      ggtitle(label = "Participant Counts by ZIP Code",
+              subtitle = "March 2017 to Present")
+  }, height = 600)
+  
+}
+
+shinyApp(ui, server)
+
+
+
+
---
+++ UDSEnrollmentDashboardCron/get_mindset_data.R
@@ -0,0 +1,245 @@
+#!/usr/bin/env Rscript
+
+# ETL for Mindset Registry 3/2017 report + UDS 2.0 IDs report
+
+library(RCurl)
+library(jsonlite)
+library(dplyr)
+library(tidyr)
+
+# operational <- TRUE
+deployed <- TRUE
+
+# if (operational) {
+#   source(paste0(path_to_app, "config.R"), local = TRUE)
+# } else {
+#   source(paste0(path_to_app, "UDSEnrollmentDashboardCron/config.R"), local = TRUE)
+# }
+
+if (deployed) {
+  path_to_app <-
+    "~/ShinyApps/MADCDashboard/" # Michigan Medicine R Shiny server
+} else {
+  path_to_app <-
+    "~/Documents/GitHub/UDSEnrollmentDashboard/UDSEnrollmentDashboardCron/" # local
+}
+
+source(paste0(path_to_app, "config.R"), local = TRUE)
+
+# get_data_mindset <- function() {
+
+# # # # # 
+## Retrieve data from REDCap API ----
+
+## Retrieve MiNDSET Registrty 3/2017 data from R/C API
+## ... keep only relevant field(s)
+# df_mindset <-  # build / debug
+df_mindset_xfrm <-  # operational
+  fromJSON(
+    postForm(
+      uri = API_URL,
+      token = MINDSET_API_TOKEN,
+      content = 'report',
+      format = 'json',
+      report_id = MINDSET_REPORT_ID,
+      rawOrLabel = 'label',
+      rawOrLabelHeaders = 'label',
+      exportCheckboxLabel = 'false',
+      returnFormat = 'json',
+      # .opts = list(ssl.verifypeer = TRUE, verbose = TRUE) # can't use now*
+      .opts = list(ssl.verifypeer = FALSE, verbose = TRUE)
+    )
+  ) %>% 
+  dplyr::select(-pt_deceased, -withdrew_date)
+
+## Retrieve UDS 2.0 IDs data from R/C API
+## ... keep only relevant field(s)
+df_uds2_id <- 
+  fromJSON(
+    postForm(
+      uri = API_URL,
+      token = UDS2_API_TOKEN,
+      content = 'report',
+      format = 'json',
+      report_id = UDS2_REPORT_ID,
+      rawOrLabel = 'label',
+      rawOrLabelHeaders = 'label',
+      exportCheckboxLabel = 'false',
+      returnFormat = 'json',
+      # .opts = list(ssl.verifypeer = TRUE, verbose = TRUE) # can't use now*
+      .opts = list(ssl.verifypeer = FALSE, verbose = TRUE)
+    )
+  ) %>% 
+  dplyr::select(subject_id) # only keep `subject_id` column
+
+
+## Copy df_mindset
+# df_mindset_xfrm <- df_mindset # build / debug
+
+# # # # #  
+## Define factor levels ----
+##   1. redcap_event_name_levels
+##   2. dx_levels
+##   3. race_levels
+##   4. sex_levels
+
+## 1. redcap_event_name_levels
+redcap_event_name_levels <- c("Baseline",
+                              paste0("Visit 0", 1:9),
+                              paste0("Visit ", 10:15))
+
+## 2. dx_levels
+dx_levels <- c("MCI", "NL", "LBD", "AD", "Impaired, not MCI",
+               "Pending consensus", "FTD", "Withdrew",
+               "Amnestic multidomain", "Other",
+               # target diagnoses
+               "MCI target", "NL target", "LBD target",
+               "AD target", "FTD target")
+
+## 3. race_levels
+race_levels <- c("Asian", "Black", "Other", "White",
+                 "American Indian or Alaska Native",
+                 "Native Hawaiian or Other Pacific Islander")
+
+## 4. sex_levels
+sex_levels <- c("Female", "Male")
+
+# # # # # 
+## Mutate fields (if nec.); Coerce fields to appropriate classes ----
+
+## Coerce `redcap_event_name` to factor class
+df_mindset_xfrm <- df_mindset_xfrm %>%
+  dplyr::mutate(redcap_event_name =
+                  readr::parse_factor(redcap_event_name,
+                                      levels = redcap_event_name_levels))
+
+## Coerce `exam_date` to Date class
+df_mindset_xfrm$exam_date <- lubridate::ymd(df_mindset_xfrm$exam_date)
+
+## Mutate `uds_dx` and coerce to factor class
+df_mindset_xfrm <- df_mindset_xfrm %>%
+  dplyr::mutate(uds_dx = dplyr::case_when(
+    uds_dx == "Amnestic MCI-memory only"          ~ "MCI",
+    uds_dx == "Amnestic MCI-memory plus"          ~ "MCI",
+    uds_dx == "Amnestic MCI, multiple domains"    ~ "MCI",
+    uds_dx == "Amnestic MCI, single domain"       ~ "MCI",
+    uds_dx == "Amnestic multidomain dementia syndrome"
+    ~ "Amnestic multidomain",
+    uds_dx == "Dem with Lewy bodies"              ~ "LBD",
+    uds_dx == "FTD"                               ~ "FTD",
+    uds_dx == "Impaired, not MCI"                 ~ "Impaired, not MCI",
+    uds_dx == "NL"                                ~ "NL",
+    uds_dx == "Non-Amnestic MCI-multiple domains" ~ "MCI",
+    uds_dx == "Non-Amnestic MCI-single domain"    ~ "MCI",
+    uds_dx == "Other"                             ~ "Other",
+    uds_dx == "Patient never came to consensus"   ~ "Withdrew",
+    uds_dx == "Per Center Decision-patient did not come to consensus-milestoned out of study"
+    ~ "Withdrew",
+    uds_dx == "Possible AD"                       ~ "AD",
+    uds_dx == "Primary progressive aphasia"       ~ "FTD",
+    uds_dx == "Probable AD"                       ~ "AD",
+    uds_dx == "Vascular dem"                      ~ "Other",
+    is.na(uds_dx) & comp_withd == "Y"             ~ "Withdrew",
+    uds_dx == "" & comp_withd == "Y"              ~ "Withdrew",
+    is.na(uds_dx) & is.na(comp_withd)             ~ "Pending consensus",
+    uds_dx == "" & comp_withd == ""               ~ "Pending consensus",
+    is.na(uds_dx) & comp_withd == ""              ~ "Pending consensus",
+    uds_dx == "" & is.na(comp_withd)              ~ "Pending consensus",
+    is.na(uds_dx) | uds_dx == ""                  ~ "Other",
+    TRUE ~ uds_dx
+  )) %>%
+  dplyr::mutate(uds_dx =
+                  readr::parse_factor(uds_dx,
+                                      levels = dx_levels))
+
+## Mutate `race_value` and coerce to factor class
+df_mindset_xfrm <- df_mindset_xfrm %>%
+  dplyr::mutate(race_value = dplyr::case_when(
+    race_value == "Asian"    ~ "Asian",
+    race_value == "Black"    ~ "Black",
+    race_value == "Other"    ~ "Other",
+    race_value == "White"    ~ "White",
+    race_value == "Hispanic" ~ "Other",
+    race_value == ""         ~ "Other",
+    is.na(race_value)        ~ "Other"
+  )) %>%
+  dplyr::mutate(race_value =
+                  readr::parse_factor(race_value,
+                                      levels = race_levels))
+
+## Coerce `sex_value` to factor class
+df_mindset_xfrm <- df_mindset_xfrm %>%
+  dplyr::mutate(sex_value =
+                  forcats::as_factor(sex_value, levels = sex_levels))
+
+## Coerce `comp_withd` to factor class
+df_mindset_xfrm <- df_mindset_xfrm %>%
+  dplyr::mutate(comp_withd =
+                  forcats::as_factor(comp_withd))
+
+## Coerce `blood_drawn` to factor class
+df_mindset_xfrm <- df_mindset_xfrm %>%
+  dplyr::mutate(blood_drawn =
+                  forcats::as_factor(blood_drawn))
+
+## Coerce `consent_to_autopsy` to factor class
+df_mindset_xfrm <- df_mindset_xfrm %>%
+  dplyr::mutate(consent_to_autopsy =
+                  forcats::as_factor(consent_to_autopsy))
+
+## Coerce `mri_completed` to factor class
+df_mindset_xfrm <- df_mindset_xfrm %>%
+  dplyr::mutate(mri_completed =
+                  forcats::as_factor(mri_completed))
+
+## Leave `county` field as character class
+
+## Coerce `birth_date` to Date class
+df_mindset_xfrm <- df_mindset_xfrm %>%
+  dplyr::mutate(birth_date =
+                  lubridate::ymd(birth_date))
+
+## Mutate `zip_code` to only include first 5 digits
+df_mindset_xfrm <- df_mindset_xfrm %>%
+  dplyr::mutate(zip_code = stringr::str_sub(zip_code, 1, 5))
+
+## Arrange and clean `df_mindset_xfrm` ----
+
+## Arrange data frame by subject_id (1st), then exam_date (2nd)
+df_mindset_xfrm <- df_mindset_xfrm %>%
+  dplyr::arrange(subject_id, exam_date)
+
+## Clean out duplicate visits ... keeping oldest visit
+duplicated_subject_ids <- duplicated(df_mindset_xfrm$subject_id)
+df_mindset_xfrm <- df_mindset_xfrm %>%
+  dplyr::filter(!duplicated_subject_ids)
+
+# # # # # 
+## Process `df_uds2_id`` ----
+## ... add a simple `uds_version` character column
+###
+df_uds2_id <- df_uds2_id %>% 
+  dplyr::mutate(uds_version = "UDS 2/3")
+
+# # # # # 
+## Left join `df_mindset_xfrm`` and `df_uds2_id` ----
+###
+df_mindset_xfrm <- df_mindset_xfrm %>% 
+  dplyr::left_join(df_uds2_id, by = "subject_id") %>% 
+  dplyr::mutate(uds_version = 
+                  ifelse(is.na(uds_version), "UDS 3", uds_version))
+
+# # # # # 
+## Return xformed MiNDSET data ----
+# return(df_mindset_xfrm)
+# }
+
+saveRDS(df_mindset_xfrm, paste0(path_to_app, "rds/df_mindset_xfrm.Rds"))
+
+# * The REDCap server for some reason isn't able to verify the Shiny server
+#   SSL certificate. Andrew Carroll's (MICHR) recommendation is to bypass
+#   the SSL certificate verfication... "for the time being".
+
+
+
+
